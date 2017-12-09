cc4bf697341234cf7f86a383bbe4e54ca53fec53
mdiannna <marusicdiana@gmail.com>
2017-12-09 14:44:32 +0200

    Version 1 done:

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ function myFunction() {
 }
 
 function displayLetter() {
+	var prenume = document.getElementById('prenume').value;
+	if(!prenume || prenume == "") {
+		alert("Scrie-ti prenumele te rog :)");
+		return;
+	}
+
 	document.getElementById('camp').style.display = "none";
 	document.getElementById('mesaj').style.display = "block";
 	document.getElementById('mesajChallenge').style.display = "none";
 
 	var nume = document.getElementById('nume').value;
-	var prenume = document.getElementById('prenume').value;
+	
 
 	nameForLetter = document.getElementById("numeScrisoare");
 	nameForLetter.innerHTML = "Draga " + prenume + ",";
