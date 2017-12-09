75e5cec59599ec7c16add4fbbaca3835f71a705a
mdiannna <marusicdiana@gmail.com>
2017-12-09 14:25:01 +0200

    combined js+html

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ function myFunction() {
 }
 
 function displayLetter() {
+	document.getElementById('camp').style.display = "none";
+	document.getElementById('mesaj').style.display = "block";
+	document.getElementById('mesajChallenge').style.display = "none";
+
 	var nume = document.getElementById('nume').value;
 	var prenume = document.getElementById('prenume').value;
 
 	nameForLetter = document.getElementById("numeScrisoare");
 	nameForLetter.innerHTML = "Draga " + prenume + ",";
-	displayRandomChallenge();
-
 }
 
 
@@ -21,6 +23,10 @@ nrOfChallenges = 10;
 function displayRandomChallenge() {
 	var challengeNr = Math.floor(Math.random() * nrOfChallenges + 1);
 	console.log(challengeNr);
+
+	document.getElementById('camp').style.display = "none";
+	document.getElementById('mesaj').style.display = "none";
+	document.getElementById('mesajChallenge').style.display = "block";
 
 	var nrChallengeElem = document.getElementById("nrChallenge");
 	nrChallengeElem.innerHTML = challengeNr;
