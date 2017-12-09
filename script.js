f4d403cd23f32f1f585c9bd75926523515765d4d
mdiannna <marusicdiana@gmail.com>
2017-12-09 12:08:46 +0200

    added js functions

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function myFunction() {
- 	var nume= document.getElementById('nume').value;
-	var prenume= document.getElementById('prenume').value;
-	document.getElementById ('nume').value =' ';
-	document.getElementById ('prenume').value =' ';
-	
+ 	var nume = document.getElementById('nume').value;
+	var prenume = document.getElementById('prenume').value;
+	// document.getElementById ('nume').value =' ';
+	// document.getElementById ('prenume').value =' ';
+}
+
+function displayLetter() {
+	var nume = document.getElementById('nume').value;
+	var prenume = document.getElementById('prenume').value;
+
+	nameForLetter = document.getElementById("numeScrisoare");
+	nameForLetter.innerHTML = "Draga " + nume + ",";
+}
+
+
+nrOfChallenges = 10;
+
+function displayRandomChallenge() {
+	int challengeNr = Math.floor(Math.random() * nrOfChallenges + 1);
+	console.log(challengeNr);
 }
